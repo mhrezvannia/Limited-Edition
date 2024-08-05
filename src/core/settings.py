82dec7c06a2bb582e24798272686aa0ed61e56c1
Mohammad Rezvan <164409154+mhrezvannia@users.noreply.github.com>
2024-08-05 12:01:01 +0330

    create generals
    create general templates directory
    create general statics directory
    create general media directory

--- src/core/settings.py
+++ src/core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website.apps.WebsiteConfig',
+    'customers.apps.CustomersConfig',
+    'accounts.apps.AccountsConfig',
+    'orders.apps.OrdersConfig',
+    'vendors.apps.VendorsConfig',
+    'dashboard.apps.DashboardConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
