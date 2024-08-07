56faaa9acf7f06594831c9ba82fca6c849b1fe8b
Mohammad Rezvan <164409154+mhrezvannia@users.noreply.github.com>
2024-08-07 19:13:44 +0330

    create customer forms
    +create forms.py
    create customer form
    create customers url.py

---
+++ src/customers/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm,
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.contrib.auth import authenticate
+
+from accounts.models import User
+
+
+class CustomerForm(forms.ModelForm):
+
+    password2 = forms.CharField(label='Password_confirm', widget=forms.PasswordInput, max_length=20)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password2")
+        if password1 != password2:
+            raise forms.ValidationError("passwords doesn't match")
+        return password2
+    
