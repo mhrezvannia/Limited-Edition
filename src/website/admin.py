18c83de27e33d81f1ab885a65584104a9a029795
Mohammad Rezvan <164409154+mhrezvannia@users.noreply.github.com>
2024-08-07 16:38:50 +0330

    create website app
    + product model
    + category model
    + comment model
    + rate model
    + product category model
    + product picture model

--- src/website/admin.py
+++ src/website/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'has_discount',
+                    'discount_type', 'discount_value',
+                    'vendor', 'is_active')
+    list_filter = ('vendor',)
+
+
+@admin.register(ProductPicture)
+class ProductPicture(admin.ModelAdmin):
+    list_display = ('file_name', 'product')
+
+
+@admin.register(Rate)
+class Rate(admin.ModelAdmin):
+    list_display = ('product', 'customer', 'star_count')
+
+
+@admin.register(Comment)
+class Comment(admin.ModelAdmin):
+    list_display = ('product', 'customer', 'approved')
--- src/website/models.py
+++ src/website/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from accounts.models import User
+from customers.models import Address
+from vendors.models import Vendor
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    has_discount = models.BooleanField(default=False)
+    discount_type = models.IntegerField(default=0)
+    discount_value = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductPicture(models.Model):
+    file_name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    alt = models.CharField(max_length=255, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.file_name
+
+
+class Rate(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    star_count = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.customer.email} - {self.product.title}'
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.customer.email} - {self.product.title}'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    image_url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductCategory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('product', 'category')
