1e5f0e6cb854b9d9afe7847df471e52f1f8d9992
Mohammad Rezvan <164409154+mhrezvannia@users.noreply.github.com>
2024-08-14 10:06:29 +0330

    Update models
    +create new custom user
    +create new customer user

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 
-
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 
 # class UserManager(BaseUserManager):
@@ -39,45 +38,73 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email=None, phone_number=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
         return self.create_user(email, phone_number, password, **extra_fields)
 
-    
-class User(AbstractBaseUser):
-    email = models.EmailField(unique=True, max_length=50)
-    phone = models.CharField(max_length=50, unique=True)
-    first_name = models.CharField(max_length=255, null=True)
-    last_name = models.CharField(max_length=255, null=True)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
 
-    # ROLES
-    CUSTOMER = 0
-    OWNER = 1
-    MANAGER = 2
-    OPERATOR = 3
+# class User(AbstractBaseUser):
+#     email = models.EmailField(unique=True, max_length=50)
+#     phone = models.CharField(max_length=50, unique=True)
+#     first_name = models.CharField(max_length=255, null=True)
+#     last_name = models.CharField(max_length=255, null=True)
+#     is_active = models.BooleanField(default=True)
+#     is_admin = models.BooleanField(default=False)
+#
+#     # ROLES
+#     CUSTOMER = 0
+#     OWNER = 1
+#     MANAGER = 2
+#     OPERATOR = 3
+#
+#     ROLE_CHOICES = (
+#         (OWNER, "Owner"),
+#         (CUSTOMER, 'Customer'),
+#         (MANAGER, 'Manager'),
+#         (OPERATOR, 'Operator')
+#     )
+#     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
+#
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = []
+#
+#     objects = UserManager()
+#
+#     def __str__(self):
+#         return self.email
+#
+#     @property
+#     def is_staff(self):
+#         return self.is_admin
+#
+#     def has_module_perms(self, app_label):
+#         return True
+#
+#     def has_perm(self, perm, obj=None):
+#         return True
 
-    ROLE_CHOICES = (
-        (OWNER, "Owner"),
-        (CUSTOMER, 'Customer'),
-        (MANAGER, 'Manager'),
-        (OPERATOR, 'Operator')
-    )
-    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
 
-    USERNAME_FIELD = "email"
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, unique=True, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    objects = UserManager()
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.email
+        return self.email if self.email else self.phone_number
 
-    @property
-    def is_staff(self):
-        return self.is_admin
 
-    def has_module_perms(self, app_label):
-        return True
+class Customer(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
 
-    def has_perm(self, perm, obj=None):
-        return True
+    def __str__(self):
+        return self.user.email
+
+
