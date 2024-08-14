881d4daba89c5235141c51b365307a1406c9f72b
Mohammad Rezvan <164409154+mhrezvannia@users.noreply.github.com>
2024-08-14 10:51:40 +0330

    add backends.py
    create a custom backend for login with email and phone number

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -18,4 +18,37 @@ class AddressForm(forms.ModelForm):
         fields = ['zip_code', 'city', 'detail']
 
 
+class CustomerSignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'phone_number', 'password1', 'password2']
+
+
+class VendorOwnerSignUpForm(UserCreationForm):
+    vendor_name = forms.CharField(max_length=255)
+    vendor_address = AddressForm()
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'phone_number', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            address = self.cleaned_data['vendor_address']
+            address.save()
+            vendor = Vendor.objects.create(
+                name=self.cleaned_data.get('vendor_name'),
+                address=address,
+                owner=user
+            )
+        return user
+
+
+class VendorEmployeeForm(forms.ModelForm):
+    class Meta:
+        model = VendorEmployee
+        fields = ['user', 'role']
+
 
