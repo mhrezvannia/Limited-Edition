ecbb21254cb7b1f2a1b51388db27a482d8c4b647
Mohammad Rezvan <164409154+mhrezvannia@users.noreply.github.com>
2024-08-15 05:31:28 +0330

    update urls.py
    +add all urls for website models

--- src/website/urls.py
+++ src/website/urls.py
@@ -6,5 +6,20 @@ app_name = 'website'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
-    path('shop/', ProductListView.as_view(), name='shop'),
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/new/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('categories/new/', CategoryCreateView.as_view(), name='category_create'),
+    path('categories/<int:pk>/edit/', CategoryUpdateView.as_view(), name='category_update'),
+    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
+
+    path('rates/new/', RateCreateView.as_view(), name='rate_create'),
+    path('rates/<int:pk>/edit/', RateUpdateView.as_view(), name='rate_update'),
+    path('rates/<int:pk>/delete/', RateDeleteView.as_view(), name='rate_delete'),
+
 ]
